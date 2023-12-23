3a36129e22bc8b93f4b93e209ce9b30283621faa
Jim675 <56530557+Jim675@users.noreply.github.com>
2023-12-23 13:42:48 +0800

    111

--- BaseDialog.cpp
+++ BaseDialog.cpp
@@ -1,20 +1,5 @@
-#include "BaseDialog.h"
-
-BaseDialog::BaseDialog(QWidget* parent):QDialog(parent)
-{
-    // 去掉右上角问号
-    setWindowFlag(Qt::WindowContextHelpButtonHint, false);
-    setAttribute(Qt::WidgetAttribute::WA_DeleteOnClose, false);
-}
-
-BaseDialog::~BaseDialog()
-{
-//我是其他改回去的文件
-}
-
-
-// 设置在关闭时是否自动delete
 void BaseDialog::setAutoDelete(bool autoDelete)
 {
     setAttribute(Qt::WidgetAttribute::WA_DeleteOnClose, autoDelete);
 }
+Lqwlwqlwlqwlql
